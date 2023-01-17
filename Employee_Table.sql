1807f12503ac9537911c68eca92eba06a2ea52ed
Krushali Talaviya <krushalitalaviya@gmail.com>
2023-01-17 10:32:49 +0530

    Update

--- Employee_Table.sql
+++ Employee_Table.sql
@@ -1,7 +1,7 @@
 -- Create an “employee” database and 4 tables (hobby, employee, employee_salary, employee_hobby).
 CREATE TABLE hobby(
 	id INT NOT NULL AUTO_InCREMENT PRIMARY KEY,
-    	name VARCHAR(255) NOT NULL
+    	name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE employee (
@@ -9,14 +9,14 @@ CREATE TABLE employee (
 	first_name VARCHAR ( 30 ),
 	last_name VARCHAR ( 30 ),
     	age INT,
-    	mobile_number VARCHAR(12),
+    	mobile_number VARCHAR(17),
     	address VARCHAR(100)
 );
 
 CREATE TABLE employee_salary (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     	salary DOUBLE NOT NULL,
-    	date DATE NOT NULL,
+    	DATE DATE NOT NULL,
     	fk_employee_id INT NOT NULL,
     	FOREIGN KEY ( fk_employee_id ) REFERENCES employee(id)
 );
